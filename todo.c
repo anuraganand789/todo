957624e46a956eb9ae5132e5ae01dfee1d3712d2
Anurag Anand <anuraganand789@hotmail.com>
2019-02-09 10:53:38 +0530

    Code for reading todo from a file is complete. Code for writing todo in a file is complete

--- todo.c
+++ todo.c
@@ -3,9 +3,7 @@
 #include <string.h>
 //4Kb i guess
 #define BUF_SIZE 4048
-char content[BUF_SIZE];
 const char *todoFilePath = "/home/anuraganand/todo.do";
-
 
 //my todo file
 FILE *file = NULL;
@@ -15,33 +13,61 @@ struct Todo {
 };
 
 void main(void) {
+  const size_t charSize = sizeof(char);
 
   printf("Enter a choice :- \n");
   printf("1. Read the todo list.\n");
   printf("2. Write todo.\n");
   
-  int choice = 0;
-  scanf("%d\n", &choice);
 
-  switch(choice) {
-  case 1 :
-    //read the todo list;
-    break;
-  case 2:
-    //write todo
-    file = fopen(todoFilePath, "a");
-    if(file) {
-      char *sentence =  (char *) malloc(100 * sizeof(char));
-      fgets(sentence, 100, stdin);
-      fwrite(sentence, sizeof(char), strlen(sentence), file);
-      fflush(stdout);
-      fclose(file);
-      free(sentence);
+
+  while(1) {
+    char choice = getchar();
+
+    switch(choice) {
+    case '1' :
+      //read the todo list;
+      file = fopen(todoFilePath, "r");
+      if(file) {
+	fflush(stdout);
+	char *sentence = (char *) calloc(1024, charSize);
+	printf("size allocated.\n");
+	fflush(stdout);
+	//reads in block of 4Kb
+	unsigned int noOfItemsRead = fread(sentence, charSize, (BUF_SIZE/charSize), file);
+	printf("total %d characters read from file\n", noOfItemsRead);
+	printf("%s\n", sentence);
+	if(feof(file)) {
+	  printf("We have read entire content of this file.\n");
+	}
+	fclose(file);
+      } else {
+	printf("No todo file is there... Loser..");
+      }
+      //break from reading from file
+      break;
+    case '2':
+      //write todo
+      //get rid of newlines
+      getchar();
+      file = fopen(todoFilePath, "a");
+      if(file) {
+	char *sentence = (char *) calloc((BUF_SIZE/charSize), charSize);
+	char *s = fgets(sentence, (BUF_SIZE/charSize), stdin);
+	printf("%s\n", s);
+	fwrite(sentence, sizeof(char), strlen(sentence), file);
+	fflush(stdout);
+	free(sentence);
+	fclose(file);
+      } else {
+	printf("Looks like there is some issues with opening the file for writing.\n");
+      }
+      //break from writing to todo file
+      break;
     }
-    
-    break;
+
+    //reset choice
+    choice = '\0';
   }
-  
-  
   return;
 }
