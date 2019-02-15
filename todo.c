a2620e5bfcd692972a3b2c0af74942a398e58cbd
Anurag Anand <anuraganand789@hotmail.com>
2019-02-16 03:15:13 +0530

    menu for delete operation added

--- todo.c
+++ todo.c
@@ -5,9 +5,9 @@
 
 void main(void) {
   int modified = 0;
-
+  clearTerminal();
   initTodoList();
-  
+
   while(1) {
     displayMenu();
 
@@ -29,14 +29,25 @@ void main(void) {
     case '2':
       addNewTodo();
       //break from writing to todo file
+      clearTerminal();
+      displayTodoList(todoHead);
       break;
     case '3' :
       displayTodoList(todoHead);
       update();
+      clearTerminal();
+      displayTodoList(todoHead);
       //store in a file
       save();
       break;
-    case '4' :
+    case '4':
+      displayTodoList(todoHead);
+      delete();
+      clearTerminal();
+      displayTodoList(todoHead);
+      save();
+      break;
+    case '5' :
       //Exit
       printf("Application Closed.\n");
       return;
